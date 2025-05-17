f76d10e50e055d6e3b4f6f392373214a2c8e1fdf
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-17 15:42:13 +0900

    feat: 내 닉네임 조회 및 수정하기

--- week6/practice2/src/App.tsx
+++ week6/practice2/src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { useState } from 'react';
+
+const userId = 100;
+
+const getMyNickname = async (userId: number) => {
+  const response = await axios.get('https://api.atsopt-seminar4.site/api/v1/users/me', {
+    headers: {
+      userId: userId
+    }
+  });
+  return response.data.data;
+};
+
+const updateNickname = async (newNickname: string) => {
+  const response = await axios.patch('https://api.atsopt-seminar4.site/api/v1/users',
+    { nickname: newNickname },
+    {
+      headers: {
+        userId: userId
+      }
+    }
+  );
+  return response.data.data;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newNickname, setNewNickname] = useState('');
+  const queryClient = useQueryClient();
+
+  const { data, isPending } = useQuery({
+    queryKey: ['myNickname', userId],
+    queryFn: () => getMyNickname(userId)
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: updateNickname,
+    onSuccess: () => {
+      // 쿼리 데이터 무효화를 통한 데이터 갱신
+      queryClient.invalidateQueries({ queryKey: ['myNickname', userId] });
+      setNewNickname('');
+    }
+  });
+
+  if (isPending) return <div>로딩 중...</div>;
 
   return (
-    <>
+    <div>
+      내 정보:
+      {data && <span> {data.nickname}</span>}
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input
+          type="text"
+          value={newNickname}
+          onChange={(e) => setNewNickname(e.target.value)}
+          placeholder="새 닉네임"
+        />
+        <button
+          onClick={() => mutate(newNickname)}
+        >
+          닉네임 수정
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
