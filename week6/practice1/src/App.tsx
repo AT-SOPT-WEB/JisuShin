3ba436a0d6d5cd5303952cb600d4351db9a3c9e9
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-17 15:03:00 +0900

    feat: SOPT 회원 닉네임 조회하기

--- week6/practice1/src/App.tsx
+++ week6/practice1/src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 4. 조회가 잘 된다면? 버튼을 클릭하면 조회가 되게 해보자!
+
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useState } from 'react';
+
+const getUsers = async () => {
+  const response = await axios.get('https://api.atsopt-seminar4.site/api/v1/users');
+
+  return response.data.data;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [shouldFetch, setShouldFetch] = useState(false);
+
+  const { data, isPending } = useQuery({
+    queryKey: ['users'],
+    queryFn: getUsers,
+    enabled: shouldFetch,
+  });
+
+  const isLoading = shouldFetch && isPending;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>유저 목록</h1>
+      <button onClick={() => setShouldFetch(true)}>
+        데이터 가져오기
+      </button>
+
+      {isLoading ? (
+        <div>로딩 중...</div>
+      ) : (
+        shouldFetch && data?.nicknameList && (
+          <div>
+            {data.nicknameList.map((nickname: string, index: number) => (
+              <div key={index}>
+                <h2>{nickname}</h2>
+              </div>
+            ))}
+          </div>
+        )
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
